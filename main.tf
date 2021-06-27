cdb6c50f3e8eb334cc50e5150b8fc3f3fdefe7e2
ch1aki <sugawara256@gmail.com>
2021-06-27 12:15:33 +0900

    init

---
+++ main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+module "aws_isucon_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "aws-isucon"
+  description = "aws-isucon"
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules       = ["ssh-tcp", "http-80-tcp"]
+  egress_rules        = ["all-all"]
+}
+
+resource "aws_instance" "isucon10" {
+  ami           = "ami-0f7362c1bbc7e30ec"
+  instance_type = "t3.micro"
+  key_name      = "isucon"
+
+  vpc_security_group_ids = [module.aws_isucon_sg.security_group_id]
+
+  tags = {
+    Name = "ISUCON10"
+  }
+}
+
